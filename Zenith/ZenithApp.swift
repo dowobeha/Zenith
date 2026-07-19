2c0533b1ac33d5eaaf400b75ca735b355e742100
Lane Schwartz <dowobeha@gmail.com>
2026-07-19 11:49:54 -1100

    Initial Commit

---
+++ Zenith/ZenithApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZenithApp.swift
+//  Zenith
+//
+//  Created by Lane Woodrose Schwartz on 7/19/26.
+//
+
+import SwiftUI
+
+@main
+struct ZenithApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
